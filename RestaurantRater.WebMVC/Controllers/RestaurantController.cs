05ec3e3ad2f07b60c719df9d4728dcf6813177f0
Laura Garcia <lgarcia223@gmail.com>
2019-02-25 16:18:51 -0500

    Added boxes for address and rating

--- RestaurantRater.WebMVC/Controllers/RestaurantController.cs
+++ RestaurantRater.WebMVC/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater.WebMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,23 @@ namespace RestaurantRater.WebMVC.Controllers
         public ActionResult Index()
         {
             return View(db.Restaurants.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Post: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID, Name, Address, Rating")]Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            { 
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
